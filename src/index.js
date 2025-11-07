9c6c15e0d8c5e151c498269ef054b4a00b73cb82
HorrySheet11 <marinnathan11@gmail.com>
2025-11-07 10:21:54 +0800

    implementing add toDo item
    error on append child in toDo.js addToDOM()

--- src/index.js
+++ src/index.js
@@ -7,3 +7,28 @@ try {
 } catch (error) {
     console.log(error);
 }
+
+const titleInput = document.getElementById('toDo-title');
+const descriptionInput = document.getElementById('toDo-description');
+const dueDateInput = document.getElementById('toDo-dueDate');
+const priorityInput = document.getElementById('toDo-priority');
+
+const toDoDialog = document.getElementById('add-toDo-dialog');
+const submitButton = document.getElementById('submit');
+submitButton.addEventListener('click', () => {
+    const title = titleInput.value;
+    const description = descriptionInput.value;
+    const dueDate = dueDateInput.value;
+    const priority = priorityInput.value;
+    const newTodo = new ToDo(title, description, dueDate, priority);
+    newTodo.addToDo();
+
+    closeDialog();
+})
+
+function closeDialog() {
+    toDoDialog.close();
+    titleInput.value = descriptionInput.value = dueDateInput.value = priorityInput.value = '';
+}
+const addToDo = document.getElementById('add-toDo'); 
+addToDo.addEventListener('click', () => toDoDialog.showModal());
--- src/toDo.js
+++ src/toDo.js
@@ -1,4 +1,5 @@
 let toDos = [];
+
 export class ToDo {
   constructor(title, description, dueDate, priority) {
     this.title = title;
@@ -13,24 +14,22 @@ export class ToDo {
     this.completed = !this.completed;
   }
 
-  addToDo(title, description, dueDate, priority) {
-    const newToDo = new ToDo(title, description, dueDate, priority);
+  addToDo() {
+    const newToDo = new ToDo(this.title, this.description, this.dueDate, this.priority);
     toDos.push(newToDo);
     this.addToDOM();
   }
 
   addToDOM() {
     const toDoList = document.getElementById('toDo-list');
-
-    toDoList.map((toDo) => {
+    toDos.map((toDo) => {
       const toDoItem = document.createElement('div');
-      toDoItem.classList.add('toDo-item');
+      toDoItem.classList.add('toDo-items');
       toDoItem.innerHTML = `
-        <input type="checkbox" id="completed" ${toDo.completed ? 'checked' : ''} onclick="toggleCompleted(${toDo.id})">
-        <h3>${toDo.title}</h3>
-        <h4>${toDo.description}</h4>
-        <p>Due: ${toDo.dueDate}</p>
-        <p>Priority: ${toDo.priority}</p>
+        <h3 id="title">${toDo.title}</h3><input id="completed" type="checkbox" />
+        <h4 id="date">${toDo.dueDate}</h4>
+        <h4 id="priority">${toDo.priority}</h4>
+        <p id="description">${toDo.description}</p>
       `;
       toDoList.appendChild(toDoItem);
     });
